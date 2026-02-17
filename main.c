975740f1b4f31c4436e086f21e665402edb65e14
Tatiana <tanya.krok@mail.ru>
2026-02-17 23:08:59 +0700

    added checking existence of brackets

--- main.c
+++ main.c
@@ -6,21 +6,28 @@ int main()
     scanf("%s", str);
 
     int cnt = 0;
+    int cnt_all = 0;
     int i = 0;
     while ((str[i]) && (cnt>=0))
     {
         if (str[i] == '(')
         {
             cnt++;
+            cnt_all++;
         }
         else if (str[i] == ')')
         {
             cnt--;
+            cnt_all++;
         }
         i++;
     }
 
-    if (cnt == 0)
+    if (cnt_all == 0)
+    {
+        printf("No brackets\n\n");
+    }
+    else if (cnt == 0)
     {
         printf("TRUE: balance of brackets\n\n");
     }
