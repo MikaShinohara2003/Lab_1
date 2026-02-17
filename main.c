3393e9d34b4665f3a3b9050b2002617f77e2b2a4
Tatiana <tanya.krok@mail.ru>
2026-02-17 12:00:59 +0700

    count number of brackets

--- main.c
+++ main.c
@@ -4,6 +4,22 @@ int main()
 {
     char str[50];
     scanf("%s", str);
-    printf("%s\n", str);
+
+    int cnt = 0;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '(')
+        {
+            cnt++;
+        }
+        else if (str[i] == ')')
+        {
+            cnt--;
+        }
+        i++;
+    }
+
+    printf("%d\n", cnt);
     return 0;
 }
