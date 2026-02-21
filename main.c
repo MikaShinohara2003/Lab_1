06dc4286408a5bf70dfcc911ef1049e6d2036b5b
Tatiana <tanya.krok@mail.ru>
2026-02-21 14:02:43 +0700

    convert string to number

--- main.c
+++ main.c
@@ -4,6 +4,15 @@ int main()
 {
     char str[50];
     scanf("%s", str);
-    printf("%s\n", str);
+
+    int num = 0;
+    int i = 0;
+    while (str[i])
+    {
+        num = num*10 + (str[i]-'0');
+        i++;
+    }
+
+    printf("%d\n", num);
     return 0;
 }
