b42d5957604a0c57ebd59273b16fea26194f4cdb
Tatiana <tanya.krok@mail.ru>
2026-02-21 20:59:51 +0700

    added count of words begin and end same letter

--- main.c
+++ main.c
@@ -4,6 +4,25 @@ int main()
 {
     char str[50];
     scanf("%[^\n]", str);
-    printf("%s\n\n", str);
+
+    int i = 0;
+    int cnt = 0;
+    char first_w = str[0];
+    char second_w;
+    while (str[i])
+    {
+        if (str[i] == ' ')
+        {
+            second_w = str[i-1];
+            if (first_w == second_w)
+            {
+                cnt++;
+            }
+            first_w = str[i+1];
+        }
+        i++;
+    }
+
+    printf("%d\n\n", cnt);
     return 0;
 }
