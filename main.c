7b90564dd8c9470b3dfe887f76d51279ba79474e
Tatiana <tanya.krok@mail.ru>
2026-02-24 11:29:14 +0700

    tests

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 
 int main()
 {
-    char str[50];
-    scanf("%[^.]", str);
+    char str[50] = "sts."; // первый символ равен последнему
+    //char str[50] = "stsk."; // первый символ не равен последнему
+    //char str[50] = "sts in sts."; // пробелы между словами
+    //char str[50] = "sts,in,sts."; // запятые между словами
+    //char str[50] = "sts in,sts."; // или пробел, или запятая между словами
 
     int i = 0;
     int cnt = 0;
     char first_w = str[0];
     char second_w;
-    while (str[i])
+    while (str[i] != '.')
     {
         if ((str[i] == ' ') || (str[i] == ','))
         {
@@ -28,6 +31,6 @@ int main()
         cnt++;
     }
 
-    printf("%d\n\n", cnt);
+    printf("Number of words: %d\n\n", cnt);
     return 0;
 }
