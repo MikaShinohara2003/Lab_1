7aec30a83b9d00632e9aa8e13d02a185dbfe4d38
Tatiana <tanya.krok@mail.ru>
2026-02-26 21:10:14 +0700

    added function

--- main.c
+++ main.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
 
-int main()
+int cnt_w_same_ltr(char str[])
 {
-    //char str[50] = "sts."; // первый символ равен последнему (1)
-    //char str[50] = "stsk."; // первый символ не равен последнему (0)
-    //char str[50] = "sts in sts."; // пробелы между словами (2)
-    //char str[50] = "sts,in,sts."; // запятые между словами (2)
-    //char str[50] = "sts in,sts."; // или пробел, или запятая между словами (2)
-    //char str[50] = "sts in   sts."; // несколько пробелом между словами (2)
-    //char str[50] = "sts,,,,,in,sts."; // несколько запятых между словами (2)
-    //char str[50] = "sts ,,,,  ,, in,,   ,,sts."; // несколько запятых и несколько пробелов между словами(2)
-    //char str[50] = "  sts in sts."; // несколько пробелов впереди(2)
-    //char str[50] = ",,,sts in sts."; // несколько запятых впереди(2)
-    char str[50] = "sts in sts"; // строка без точки
-
     int i = 0;
     int cnt = 0;
     char first_w;
     char second_w;
-    char fl_dot = '0';
     while ((str[i] == ' ') || (str[i] == ','))
     {
         i++;
     }
     first_w = str[i];
     while (str[i] != '\0')
-    {   
+    {
         if ((str[i] == ' ') || (str[i] == ','))
         {
             second_w = str[i-1];
@@ -43,19 +30,40 @@ int main()
 
         if (str[i] == '.')
         {
-            fl_dot = '1';
-            str[i] = '\0';
+            second_w = str[i-1];
+            if (first_w == second_w)
+            {
+                cnt++;
+            }
+
+            return cnt;
+            //str[i] = '\0';
         }
     }
-    second_w = str[i-1];
-    if (first_w == second_w)
-    {
-        cnt++;
-    }
 
-    if (fl_dot == '1')
+    cnt = -1;
+    return cnt;
+}
+
+int main()
+{
+    char str_origin[50] = "sts."; // первый символ равен последнему (1)
+    //char str[50] = "stsk."; // первый символ не равен последнему (0)
+    //char str[50] = "sts in sts."; // пробелы между словами (2)
+    //char str[50] = "sts,in,sts."; // запятые между словами (2)
+    //char str[50] = "sts in,sts."; // или пробел, или запятая между словами (2)
+    //char str[50] = "sts in   sts."; // несколько пробелом между словами (2)
+    //char str[50] = "sts,,,,,in,sts."; // несколько запятых между словами (2)
+    //char str[50] = "sts ,,,,  ,, in,,   ,,sts."; // несколько запятых и несколько пробелов между словами(2)
+    //char str[50] = "  sts in sts."; // несколько пробелов впереди(2)
+    //char str[50] = ",,,sts in sts."; // несколько запятых впереди(2)
+    //char str[50] = "sts in sts"; // строка без точки
+
+    int cnt_w = cnt_w_same_ltr(str_origin);
+
+    if (cnt_w != -1)
     {
-        printf("Number of words: %d\n\n", cnt);
+        printf("Number of words: %d\n\n", cnt_w);
     }
     else
     {
