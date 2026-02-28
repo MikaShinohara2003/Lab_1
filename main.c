5aff8150283cf854d4e8ab79086e7597266936fc
Tatiana <tanya.krok@mail.ru>
2026-03-01 00:20:18 +0700

    added function converting string to number

--- main.c
+++ main.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 
-int strToNum(char str[])
+int strToNum(char str[], int i)
 {
-
-}
-
-int main()
-{
-    char str[50] = "-120";
-
     int num = 0;
-    int i = 0;
-    char flag = '1';
-    char flag_minus = '0';
 
-    if ((str[0] == '-') && (str[1] != '\0'))
-    {
-        i++;
-        flag_minus = '1';
-    }
-    if ((str[0] == '+') && (str[1] != '\0'))
-    {
-        i++;
-    }
-
-    while ((str[i]) && (flag == '1'))
+    while (str[i])
     {
         if ((str[i] < '0') || (str[i] > '9'))
         {
-            flag = '0';
+            return -1;
         }
         else
         {
@@ -37,18 +17,47 @@ int main()
         i++;
     }
 
-    if (flag_minus == '1')
+    return num;
+}
+
+int main()
+{
+    //char str_origin[50] = "120"; // 120
+    //char str_origin[50] = "-120"; // -120
+    //char str_origin[50] = "+120"; // 120
+    //char str_origin[50] = "000120"; // 120
+    //char str_origin[50] = "-000120"; // -120
+    //char str_origin[50] = "+000120"; // 120
+    //char str_origin[50] = "-"; // can't be converted
+    //char str_origin[50] = "+"; // can't be converted
+    char str_origin[50] = "12v0"; // can't be converted
+
+    int i = 0;
+    char flag_minus = '0';
+
+    if ((str_origin[0] == '-') && (str_origin[1] != '\0'))
     {
-        num = 0 - num;
+        i++;
+        flag_minus = '1';
+    }
+    if ((str_origin[0] == '+') && (str_origin[1] != '\0'))
+    {
+        i++;
     }
 
-    if (flag == '1')
+    int res_num = strToNum(str_origin, i);
+
+    if (res_num != -1)
     {
-        printf("%d\n", num);
+        if (flag_minus == '1')
+        {
+            res_num = 0 - res_num;
+        }
+        printf("%d\n\n", res_num);
     }
     else
     {
-        printf("Symbols can't be converted\n");
+        printf("Symbols can't be converted\n\n");
     }
     return 0;
 }
